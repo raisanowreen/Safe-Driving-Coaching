0617e6e3474b5683ff739b7ae4e4b101afaca02a
Your Name <you@example.com>
2021-10-04 18:40:04 +0600

    services

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -16,9 +16,9 @@ const Portfolio = () => {
         <div>
 {/* portfolio download */}
           <div>
-          <Card className="text-black m-5">
+          <Card className="text-black fw-bolder m-5">
   <Card.Img src="https://www.sumitomocorp.com/jp/-/media/Images/hq/about/message/message-main_2021a.jpg?h=1080&la=en&w=1920" alt="Card image" />
-  <Card.ImgOverlay className="text-center mt-5 p-5 text-success"> 
+  <Card.ImgOverlay className="text-center p-5 fw-bolder text-success"> 
     <Card.Title>Welcome to Safe Driving</Card.Title>
     <Card.Text>
     <Button className="mt-3" variant="outline-info">Download Portfolio</Button>{' '}
